85251139343d8b657e2232374a7b4128063681d7
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-05-18 13:33:43 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import Hero from './Components/Hero'
+import Section from './Components/Section'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <Hero/>
+      
     </>
   )
 }
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import Section from './Section';
 import weeknd from "../Components/weeknd-2.png"
+import { ScrollParallax } from 'react-just-parallax';
+
 
 const Hero = () => {
   return (
    <Section id="hero">
     {/* main wrapper */}
-    <div >
+    <div className='flex column overflow-hidden' >
         {/* text */}
-        <div>
-            <h1 className='font-niagra text-n-2  text-5xl overflow-hidden'>The Weeknd</h1>
+        <ScrollParallax isAbsolutelyPositioned>
+        <div className=''>
+            <h1 className='absolute font-niagra text-center text-n-2  text-[17rem] overflow-hidden ml-[35rem] overflow-hidden'>The Weeknd</h1>
         </div>
+        </ScrollParallax>
         {/* image */}
-        <div>
-            <img src = {weeknd} height={20} className=' absolute max-w-[30rem] lg:max-w-[34rem] left-[35%] z-50'/>
+        <div className=''>
+            <img src = {weeknd} height={20} className=' absolute max-w-[30rem] lg:max-w-[34rem] left-[35%] z-50 mt-[6rem]'/>
         </div>
     </div>
 
