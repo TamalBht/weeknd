13dee141cb83b30ec5e9682026b4a29b68e5bbe5
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-05-22 01:24:42 +0530

    changes

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
     <div className=' absolute md:relative translate-y-[100vh] md:translate-y-0 h-fit md:h-[100vh]   top-[10vh] md:top[0vh] w-auto'>
         <nav className=' relative  md:top-0 md:mt-[10rem] ml-[2.5rem] h-[20vh] md:h-auto'>
             <ul className=' flex  md:block'>
-                <li className='mb-[3rem] rounded-full hover:shadow-[5px_5px_5px_#ff0909] px-3'>
+                <li className='w-fit mb-[3rem]  rounded-full hover:shadow-[5px_5px_5px_#ff0909] px-3'>
                     <img src={afterhour} width={100}/>
                 </li>
                 <li className='w-fit mb-[3rem] rounded-full  hover:shadow-[5px_5px_5px#274F5F]'>
