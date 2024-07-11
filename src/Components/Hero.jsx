41131a4d0975d9c1b14db24cede99167523ba1e5
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-07-11 10:19:13 +0530

    changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,30 +9,11 @@ import melancholy from "../assets/melancholy.jpg"
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 import { useGSAP } from '@gsap/react';
-import { useRef } from 'react';
+import { useRef,useEffect } from 'react';
 gsap.registerPlugin(ScrollTrigger);
 const Hero = () => {
   const firstRef=useRef(null);
-  useGSAP(()=>{
-    gsap.to(".hText",{
-      scrollTrigger:{
-        trigger:".hText",
-        start:'top centre',
-        end:"+=300",
-        scrub:true,
-        markers:false,
-        toggleActions:"play pause reverse pause",
-
-      },
-      y:200,
-      letterSpacing:15,
-     
-        duration:2,
-      
-      
-    })
-
-  })
+  
   useGSAP(()=>{
     gsap.to(firstRef.current,{
       scrollTrigger:{
@@ -53,6 +34,27 @@ const Hero = () => {
     })
 
   })
+  useEffect(()=>{
+    gsap.to(".hText",{
+      scrollTrigger:{
+        trigger:".hText",
+        start:'top centre',
+        end:"+=300",
+        scrub:true,
+        markers:false,
+        toggleActions:"play pause reverse pause",
+        
+
+      },
+      y:200,
+      letterSpacing:15,
+     
+        duration:2,
+      
+      
+    })
+
+  },[])
   return (
     <div className='flex md:block flex-col justify-evenly items-center'>
    <section id="hero" className={"mt-[0rem] mb-[40vh]"}>
@@ -60,7 +62,7 @@ const Hero = () => {
     {/* side bar */}
     <div className='md:flex    ' >
     <div className=' absolute md:relative translate-y-[100vh] md:translate-y-0 h-fit md:h-[100vh]   top-[10vh] md:top[0vh] w-auto'>
-        <nav className=' relative  md:top-0 md:mt-[10rem] ml-[2.5rem] h-[20vh] md:h-auto fixed'>
+        <nav className=' relative  md:top-0 md:mt-[10rem] ml-[2.5rem] h-[20vh] md:h-auto '>
             <ul className=' gap-3 flex justify-between md:block'>
                 <li className='w-fit mb-[3rem]  rounded-full hover:shadow-[5px_5px_5px_#ff0909] px-3'>
                    <a href="#hero"> <img src={afterhour} width={100} className='rounded-full'/></a>
